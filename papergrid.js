231e927e8771e49ae69408fe78de86c4c667b9a1
shansayan <152245999+shansayan@users.noreply.github.com>
2023-12-06 12:40:21 +0530

    Add files via upload

---
+++ papergrid.js
@@ -0,0 +1,90 @@
+function changeimg1(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-11.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-1.webp' 
+    }
+
+}
+function changeimg2(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-22.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-2.webp' 
+    }
+
+}
+function changeimg3(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-33.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-3.webp' 
+    }
+
+}
+function changeimg4(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-44.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-4.webp' 
+    }
+
+}
+function changeimg5(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-55.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-5.webp' 
+    }
+
+}
+function changeimg6(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-66.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-6.webp' 
+    }
+
+}
+function changeimg7(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-77.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-7.webp' 
+    }
+
+}
+function changeimg8(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-88.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-8.webp' 
+    }
+
+}
+function changeimg9(x,image)
+{
+    if(x==1){
+        image.src='./imgs/notebook-99.webp'
+    }
+    if(x==2){
+        image.src='./imgs/notebook-9.webp' 
+    }
+
+}
